63d98749ff3a21b1edbcab7ee36ed98eb8109eff
Sandesh C <sandeshc13@gmail.com>
2016-11-12 04:00:03 +0530

    added script for batch run on cases in test/

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for test_case in test/*
+do
+    python -c "print('*' * 50)"
+    N=$(cat $test_case | cut -d ' ' -f 1)
+    B=$(cat $test_case | cut -d ' ' -f 2)
+    printf 'Test %s\t: %s %s\n' "$test_case" "$N" "$B"
+    python pollardRhoAttack.py $N $B
+    python -c "print('*' * 50)"
+done
