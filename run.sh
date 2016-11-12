136174643f86045b414d824bbb87a9054bcca824
Sandesh C <sandeshc13@gmail.com>
2016-11-13 04:01:18 +0530

    feature: run.sh tracks runtime now

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ do
     N=$(cat $test_case | cut -d ' ' -f 1)
     B=$(cat $test_case | cut -d ' ' -f 2)
     printf 'Test %s\t: %s %s\n' "$test_case" "$N" "$B"
-    python pollardRhoAttack.py $N $B
+    /usr/bin/time -f "<< runtime %es >>" python pollardRhoAttack.py $N $B
     python -c "print('*' * 50)"
 done
